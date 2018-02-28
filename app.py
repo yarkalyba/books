9401182dc1c99cd656ccbb21d1ca8443b5980689
yarkalyba <33898599+yarkalyba@users.noreply.github.com>
2018-02-28 15:43:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_dance.contrib.facebook import make_facebook_blueprint, facebook
+from flask_dance.contrib.twitter import make_twitter_blueprint, twitter
+import random
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/yarka/Documents/PycharmProjects/books/example19.db'
@@ -36,6 +39,42 @@ def event():
     events = Event.query.all()
     return '<br>'.join([str(event) for event in events])
 
+
+
+twitter_blueprint = make_twitter_blueprint(api_key='f7dUFCVeAspsUmXBZXGLrNF8e',
+                                           api_secret='yAjRQ7CXzoOmPjfoVO2QLOnz40sqhIyU3a43WC4NdZXbLXwJMI')
+
+app.register_blueprint(twitter_blueprint, url_prefix="/twitter_login")
+
+
+facebook_blueprint = make_facebook_blueprint(client_secret='84e3f634967016f48ac1b93987cd3ac6', client_id='548449348874242')
+app.register_blueprint(facebook_blueprint, url_prefix="/facebook_login")
+
+@app.route("/facebook")
+def facebook_login():
+    if not facebook.authorized:
+        return redirect(url_for("facebook.login"))
+    account_info = facebook.get("/me")
+
+    if account_info.ok:
+        account_info_json = account_info.json()
+        return "<h1> Your twitter name is @{}".format(
+            account_info_json['screen_name'])
+    return '<h1>Request failed!</h1>'
+
+
+@app.route("/twitter")
+def twitter_login():
+    if not twitter.authorized:
+        return redirect(url_for("twitter.login"))
+    account_info = twitter.get("account/settings.json")
+
+    if account_info.ok:
+        account_info_json = account_info.json()
+        # можна там свякі штуки робити вже
+        return "<h1> Your twitter name is @{}".format(
+            account_info_json['screen_name'])
+    return '<h1>Request failed!</h1>'
 
 if __name__ == "__main__":
     app.run(debug=True)
