d2a5f52619bd44937f8b5bc9ecd13dfc736d8afd
yarkalyba <lyba@ucu.edu.edu>
2018-03-16 16:36:58 +0200

    Changes in app.py

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ import random
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'rybka{}'.format(random.randint)
 
-app.config[
-    'SQLALCHEMY_DATABASE_URI'] = \
-    "sqlite:////home/PycharmProjects/books/example19.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////home/yarka/PycharmProjects/booksexample19.db"
 
 db = SQLAlchemy(app)
 
