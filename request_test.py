1d5dd71a09fcd6c260f86c3ddee494eddb743101
yarkalyba <33898599+yarkalyba@users.noreply.github.com>
2018-05-03 19:14:13 +0300

    Delete request_test.py

--- request_test.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-# requests.post("http://yarkalyba.pythonanywhere.com/api/v0.1/likes", json={"id": "bhjfsbkjdfdkjf", "book_id": 42, "event": "liked"})
-data = {"facebook_id": "rybka", "book_id": 42, "user_id": 13, "reaction": 1}
-r = requests.post("http://localhost:5000/api/v0.1/likes", json=data)
